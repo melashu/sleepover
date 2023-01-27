18d34d82394802587f174b97199e0ac5ab2cfd5e
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-27 17:46:49 +0300

    create files for admin page

---
+++ src/Components/Admin/CreateHotel/createHotel.js
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import './createHotel.css';
+
+const createHotel = () => (
+  <div>
+    <h1>Create Hotel</h1>
+  </div>
+);
+
+export default createHotel;
