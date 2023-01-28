4ce1f8bf11579797e56b3063abe137a5a728a0ca
benjamin kisenge <benkisenge03@gmail.com>
2023-01-28 13:11:09 +0200

    Import home to app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Admin from './Components/Admin/SideRoute/sideRoute';
-import RouteAdmin from './Components/Admin/RouteAdmin';
-import Login from './Components/login/Login';
+// import Admin from './Components/Admin/SideRoute/sideRoute';
+// import RouteAdmin from './Components/Admin/RouteAdmin';
+// import Login from './Components/login/Login';
+import Home from './Components/Home/home';
 import './App.css';
 
 function App() {
   return (
     <div>
       {/* <Admin />
-      <RouteAdmin /> */}
-      <Login />
+      <RouteAdmin />
+      <Login /> */}
+      <Home />
     </div>
 
   );
