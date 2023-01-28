c5362d3218844366c04d64f776f5f2354303b62a
benjamin kisenge <benkisenge03@gmail.com>
2023-01-28 13:10:47 +0200

    Install taillwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'home-picture': "url('/src/assets/hotel.jpg')",
+      },
+      colors: {
+        'black-rgba': 'rgba(0, 0, 0, 0.54)',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
