892604ef366eee3c7dbbc2174f171e86b1127a31
benjamin kisenge <benkisenge03@gmail.com>
2023-01-30 09:51:59 +0200

    Update App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import Admin from './Components/Admin/SideRoute/sideRoute';
 // import RouteAdmin from './Components/Admin/RouteAdmin';
-// import Login from './Components/login/Login';
-import Home from './Components/Home/home';
+import Login from './Components/login/Login';
+// import Home from './Components/Home/home';
 import './App.css';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       {/* <Admin />
       <RouteAdmin />
       <Login /> */}
-      <Home />
+      <Login />
     </div>
 
   );
