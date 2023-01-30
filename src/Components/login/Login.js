d36b28f4e3f7851b4cbaa315cbae748edfbea411
benjamin kisenge <benkisenge03@gmail.com>
2023-01-30 09:50:27 +0200

    Update login page

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Login = () => (
-  <div>
+  <div className='backdrop-saturate-50 login-picture w-sreen h-screen bg-cover'>
     <h1 className="text-blue-300">Login form here</h1>
   </div>
 );
