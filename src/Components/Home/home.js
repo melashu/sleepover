4dc4ad95e9b0f26f9a9f6b4e1f81df217bc2d1ac
benjamin kisenge <benkisenge03@gmail.com>
2023-01-30 09:52:50 +0200

    Update style in home page

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -7,13 +7,13 @@ const Home = () => (
 
     </div>
     <div className='flex flex-col justify-center items-center absolute top-50  left-20' >
-      <div className='p-5 bg-black bg-opacity-25 rounded-3 w-3/6 ml-5 flex flex-col justify-center items-center'>
-        <p className='text-center text-white'>
+      <div className='p-5 bg-black bg-opacity-50 rounded-3 w-3/6 ml-5 flex flex-col justify-center items-center'>
+        <p className='text-center text-white font-pop'>
          <span className='font-extrabold'>Sleep over</span> helps you Booking rooms in advance to be ready for a meeting is called Room Booking.
           With a room booking system, people are able to search for and book a meeting
           room in advance, based on their needs and wishes.
         </p>
-        <p className='mt-5 px-5 cursor-pointer py-3 border-4 rounded-5 border-light-white-500 border-opacity-100 hover:bg-yellow-700 hover:text-white'>Book Hotel</p>
+        <p className='mt-5 px-5 text-white cursor-pointer py-3 border-4 rounded-5 border-light-white-700 border-opacity-100 hover:bg-yellow-700'>Book Hotel</p>
       </div>
     </div>
   </div>
