640b442745447018a22da2d24513d582eae5dd66
melashu <meshu102@outlook.com>
2023-01-31 11:54:01 +0300

    Update store configration

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,10 +1,15 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReduce from './users/users';
+import hotelReducer from './hotelSlices';
+import reserveReducer from './reservationSlices';
+import roomReducer from './roomSlices';
 
 const rootReducer = combineReducers({
   user: userReduce,
+  hotel: hotelReducer,
+  reserve: reserveReducer,
+  room: roomReducer,
   // Add your reducers here
-
 });
 
 const store = configureStore({
