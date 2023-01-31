e3a7b4401fae17f08604ea54488822bdf13fa593
benjamin kisenge <benkisenge03@gmail.com>
2023-01-31 15:45:23 +0200

    Fix eslints error

--- src/Redux/authentification/login.js
+++ src/Redux/authentification/login.js
@@ -1,90 +1,95 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// eslint-disable-next-line
-import axios from 'axios';
+// eslint-disable-next-line no-param-reassign
+// import axios from 'axios';
 
 const initialState = {
-  msg: "",
-  user: "",
-  token: "",
-  error: "",
+  msg: '',
+  user: '',
+  token: '',
+  error: '',
   Loading: false,
 };
 
 export const loginUser = createAsyncThunk('loginuser', async (body) => {
-  const res = await fetch("http://localhost:3001/auth/login/",{
-    method: "post",
+  const res = await fetch('http://localhost:3001/auth/login/', {
+    method: 'post',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": 'application/json'
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-    body: JSON.stringify(body)
-  })
+    body: JSON.stringify(body),
+  });
   return await res.json();
-})
+});
 
 export const signUp = createAsyncThunk('signupuser', async (body) => {
-  const res = await fetch("http://localhost:3001/auth/signup/",{
-    method: "post",
+  const res = await fetch('http://localhost:3001/auth/signup/', {
+    method: 'post',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": 'application/json'
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-    body: JSON.stringify(body)
-  })
+    body: JSON.stringify(body),
+  });
   return await res.json();
-})
+});
 
 const authSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    addToken: (state, action) => {
-      state.token = localStorage.getItem('token')
+    addToken: (state) => {
+      state.token = localStorage.getItem('token');
     },
-    addUser: (state, action) => {
-      state.user = localStorage.getItem('user')
+    addUser: (state) => {
+      state.user = localStorage.getItem('user');
     },
-    logout: (state, action) => {
+    logout: (state) => {
       state.token = null;
       localStorage.clear();
-    }
+    },
   },
   extraReducers: {
-    [signUp.pending]: (state, action) => {
-      state.Loading = true
-    },
-    [signUp.fulfilled]: (state, {payload: {error,msg}}) => {
+    [signUp.pending]: (state) => {
       state.Loading = true;
-      if(error){
-        state.error = error
+    },
+    [signUp.fulfilled]: (state, { payload: { error, msg } }) => {
+      state.Loading = true;
+      if (error) {
+        state.error = error;
       } else {
-        state.msg = msg
+        state.msg = msg;
       }
     },
-    [loginUser.rejected]: (state, action) =>  {
-      state.Loading = true
+    [loginUser.rejected]: (state) => {
+      state.Loading = true;
     },
-    /**********Log in ********************/
-    [loginUser.pending]: (state, action) => {
-      state.Loading = true
+    /** ********Log in ******************* */
+    [loginUser.pending]: (state) => {
+      state.Loading = true;
     },
-    [loginUser.fulfilled]: (state, {payload: {error,msg, user, token}}) => {
+    [loginUser.fulfilled]: (state, {
+      payload: {
+        error, msg, user, token,
+      },
+    }) => {
       state.Loading = false;
-      if(error){
-        state.error = error
+      if (error) {
+        state.error = error;
       } else {
         state.msg = msg;
         state.user = user;
         state.token = token;
-        state.error = ""
-        localStorage.setItem("msg", msg);
-        localStorage.setItem("user", JSON.stringify(user));
-        localStorage.setItem("token", token);
+        state.error = '';
+        localStorage.setItem('msg', msg);
+        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('token', token);
       }
     },
-    [loginUser.rejected]: (state, action) =>  {
-      state.Loading = true
-    }
+    [loginUser.rejected]: (state, action) => {
+      state.Loading = true;
+    },
   },
 });
 
