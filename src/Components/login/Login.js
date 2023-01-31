61eff2254e2a4ee17c1a8eca2ba2705e6e654c23
kusi <lawcubegsm@gmail.com>
2023-01-31 17:58:56 +0000

    Add sign up
    - connects the api
    - add css

--- src/Components/login/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Login = () => (
-  <div>
-    <h1>Login form here</h1>
-  </div>
-);
-
-export default Login;
--- src/Components/text_input/TextInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TextInput = () => (
-  <div>
-    <input type="text" className="text-input" name="text" />
-  </div>
-);
-
-export default TextInput;
