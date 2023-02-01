36dd620b8e94810260d63c6efb35f4d1b38aa8ab
benjamin kisenge <benkisenge03@gmail.com>
2023-02-02 00:34:21 +0200

    Fetch api for login

--- src/Redux/module/api.js
+++ src/Redux/module/api.js
@@ -1,10 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl = 'http://127.0.0.1:3000/';
+const baseUrl = 'http://127.0.0.1:3001/';
 
 const headers = {
   'Content-Type': 'application/json',
+  Accept: 'application/json',
+  Authorization: localStorage.getItem('token')
 };
 
 // const
@@ -29,6 +31,23 @@ const api = {
       }
     },
   ),
+  // Login user
+  loginUser : createAsyncThunk(
+    'loginuser', async (body) => {
+      try {
+        const res = await fetch(`${baseUrl}auth/login`, {
+          method: 'post',
+          headers,
+          body: JSON.stringify(body),
+        });
+        return await res.json();
+      } catch (error) {
+        return error
+      }
+
+  })
+
+
 
 };
 
