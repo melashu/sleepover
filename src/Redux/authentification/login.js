e1e76fd83880022a3e757a88743fdbe347fc361f
benjamin kisenge <benkisenge03@gmail.com>
2023-02-02 00:34:06 +0200

    Update login redux

--- src/Redux/authentification/login.js
+++ src/Redux/authentification/login.js
@@ -11,60 +11,13 @@ const initialState = {
   Loading: false,
 };
 
-export const loginUser = createAsyncThunk('loginuser', async (body) => {
-  const res = await fetch('http://localhost:3001/auth/login/', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
-  return await res.json();
-});
-
-export const signUp = createAsyncThunk('signupuser', async (body) => {
-  const res = await fetch('http://localhost:3001/auth/signup/', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
-  return await res.json();
-});
-
 const authSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    addToken: (state) => {
-      state.token = localStorage.getItem('token');
-    },
-    addUser: (state) => {
-      state.user = localStorage.getItem('user');
-    },
-    logout: (state) => {
-      state.token = null;
-      localStorage.clear();
-    },
+
   },
   extraReducers: {
-    [signUp.pending]: (state) => {
-      state.Loading = true;
-    },
-    [signUp.fulfilled]: (state, { payload: { error, msg } }) => {
-      state.Loading = true;
-      if (error) {
-        state.error = error;
-      } else {
-        state.msg = msg;
-      }
-    },
-    [loginUser.rejected]: (state) => {
-      state.Loading = true;
-    },
     /** ********Log in ******************* */
     [loginUser.pending]: (state) => {
       state.Loading = true;
