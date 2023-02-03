03f2383c02c7c27745f03e2f18eb427e98ff62ec
kusi <lawcubegsm@gmail.com>
2023-02-03 07:29:55 +0000

    Add reserved styling

--- src/Components/RoomDetail/roomdetail.js
+++ src/Components/RoomDetail/roomdetail.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import image from '../../assets/logo.ico';
+import logo from '../../assets/hotel.jpg'
+import colorWheel from '../../assets/color-wheel.jpg'
+import {FaAngleRight} from 'react-icons/fa';
+
 
 const RoomDetail = () => {
   const { state } = useLocation();
   return (
-    <div className="me-container d-flex bg-light">
-      <img src={state.photo.url} alt={state.room_no} />
-      <div className="m-4 flex-4">
-        <h3>
-          Room Number:
-          <strong>{state.room_no}</strong>
-        </h3>
-        <p>
-          Number of Bed:
-          <strong>{state.number_of_bed}</strong>
-        </p>
-        <p>
-          Prices:
-          <strong>
-            {state.prices}
-            $
-          </strong>
-        </p>
-        <Link to="create-reserve" state={{ id: state.id }} className="btn btn-outline-primary fs-4 me-btn">Reserve this room</Link>
+    <div className="lk-hotel-container  lk-flex">
+      <div className='lk-room-detail lk-flex'>
+        {/* // Todo: remember to change the logo */}
+        <div className='lk-reserve-room-img-wrapper lk-c-flex'>
+           {/* <img src={state.photo.url} alt={state.room_no} /> */}
+           <img src={logo} alt={state.room_no} /> 
+        </div>
+        <div className='lk-reserve-room-details-wrapper lk-c-flex'>
+         <div className="lk-reserve-room-details  flex-4">
+          
+          {/* //Todo: add hotel name in h3  */}
+           <p>
+             Room Number:
+             <strong>{state.room_no}</strong>
+           </p>
+           <p>
+             Number of Bed:
+             <strong>{state.number_of_bed}</strong>
+           </p>
+           <p>
+             Prices:
+             <strong>
+               {state.prices}
+               $
+             </strong>
+           </p>
+
+           <div className='lk-color-wheel lk-flex '>
+            <img src={colorWheel} alt='color wheel'/> 
+           </div>
+
+           <div className='lk-btn-reserve-room-wrapper lk-flex'>
+             <Link to="create-reserve" state={{ id: state.id }} className="btn ">
+                Reserve   <FaAngleRight className="fa"  />
+             </Link>
+           </div>
+
+         </div>
+        </div>
+
+
       </div>
     </div>
   );
